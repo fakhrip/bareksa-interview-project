a1adf37c31ef321ea851e72e521128afba9e29e8
fakhrip <fakhriputra123s@gmail.com>
2022-03-08 08:48:55 +0700

    refactor: testing code and add one more test

--- main_test.go
+++ main_test.go
@@ -4,12 +4,15 @@ import (
 	"bareksa-interview-project/interfaces/http/middlewares"
 	"bareksa-interview-project/interfaces/http/routes"
 	log "bareksa-interview-project/util/logger"
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/joho/godotenv"
@@ -89,21 +92,56 @@ func UnitTestSetup() *http.Server {
 
 var r = UnitTestSetup()
 
-func JSONBytesEqual(a, b []byte) bool {
+func JSONBytesEqual(a, b []byte) (bool, error) {
 	var j, j2 interface{}
+
 	if err := json.Unmarshal(a, &j); err != nil {
-		return false
+		return false, err
 	}
 	if err := json.Unmarshal(b, &j2); err != nil {
-		return false
+		return false, err
 	}
-	return reflect.DeepEqual(j2, j)
+
+	return reflect.DeepEqual(j2, j), nil
 }
 
-func TestHealthCheck(t *testing.T) {
-	req, err := http.NewRequest(http.MethodGet, "/api/v1/health_check", nil)
+func AssertBodyEqual(expected, result []byte, t *testing.T) {
+	res, err := JSONBytesEqual(expected, result)
+
+	str_a := strings.TrimSpace(string(expected))
+	str_b := strings.TrimSpace(string(result))
+
+	if res {
+		t.Logf("Expected to get body: %v, got: %v [PASS]", str_a, str_b)
+	} else {
+		if err != nil {
+			if str_a == str_b {
+				t.Logf("Expected to get body: %v, got: %v [PASS]", str_a, str_b)
+			} else {
+				t.Errorf("Expected to get body: %v, got: %v [FAIL]", str_a, str_b)
+			}
+		} else {
+			t.Errorf("Expected to get body: %v, got: %v [FAIL]", str_a, str_b)
+		}
+	}
+}
+
+func AssertResponseCodeEqual(expected, result int, t *testing.T) {
+	if expected == result {
+		t.Logf("Expected to get status code: %v, got: %v [PASS]\n", expected, result)
+	} else {
+		t.Errorf("Expected to get status code: %v, got: %v [FAIL]\n", expected, result)
+	}
+}
+
+func CreateAndSendRequest(method string, url string, body io.Reader, t *testing.T) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		t.Fatalf("Couldn't create request: %v\n", err)
+	}
+
+	if method == http.MethodPost || method == http.MethodPut {
+		req.Header.Set("Content-Type", "application/json")
 	}
 
 	// Create a response recorder so you can inspect the response
@@ -112,19 +150,27 @@ func TestHealthCheck(t *testing.T) {
 	// Perform the request
 	r.Handler.ServeHTTP(w, req)
 
-	// Check to see if the response was what you expected
-	if w.Code == http.StatusOK {
-		t.Logf("Expected to get status code: %v, got: %v [PASS]\n", http.StatusOK, w.Code)
-	} else {
-		t.Errorf("Expected to get status code: %v, got: %v [FAIL]\n", http.StatusOK, w.Code)
-	}
+	return w
+}
+
+func TestHealthCheck(t *testing.T) {
+	w := CreateAndSendRequest(http.MethodGet, "/api/v1/health_check", nil, t)
+	AssertResponseCodeEqual(w.Code, http.StatusOK, t)
 
 	expected := []byte(`{"message":"🤖: Ayy sir, service is currently healthy, you may want to continue enjoy your life now"}`)
 	result := w.Body.Bytes()
+	AssertBodyEqual(expected, result, t)
+}
 
-	if JSONBytesEqual(expected, result) {
-		t.Logf("Expected to get body: %v, got: %v [PASS]", string(expected), string(result))
-	} else {
-		t.Errorf("Expected to get body: %v, got: %v [FAIL]", string(expected), string(result))
-	}
+func TestRefreshMigrationWrongSecret(t *testing.T) {
+	migrationPass := os.Getenv("MIGRATION_PASSWORD") + "_dummy"
+	jsonStr := fmt.Sprintf(`{"secret":"%s"}`, migrationPass)
+	jsonBytes := []byte(jsonStr)
+
+	w := CreateAndSendRequest(http.MethodPost, "/api/v1/refresh_migration", bytes.NewBuffer(jsonBytes), t)
+	AssertResponseCodeEqual(w.Code, http.StatusInternalServerError, t)
+
+	expected := []byte(`The secret is wrong, dont try any harder if you are not the admin`)
+	result := w.Body.Bytes()
+	AssertBodyEqual(expected, result, t)
 }
